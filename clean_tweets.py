2f641fe576f827430685e6853ee50a045debdd83
Marlus Araujo <m@marlus.com>
2020-02-04 11:22:36 -0300

    cleanup

--- clean_tweets.py
+++ clean_tweets.py
@@ -5,20 +5,11 @@ from optparse import OptionParser
 def main():
     parser = OptionParser(usage="usage: %prog [--input tweets.csv] [--output tweets_clean.txt] [--column tweet]",
                           version="%prog 1.0")
-    parser.add_option("-i", "--input",
-	    action="store", # optional because action defaults to "store"
-	    dest="input",
-	    default="tweets.csv",
+    parser.add_option("-i", "--input", dest="input", default="tweets.csv",
 	    help="the input CSV file",)
-    parser.add_option("-c", "--column",
-	    action="store", # optional because action defaults to "store"
-	    dest="column",
-	    default="tweet",
+    parser.add_option("-c", "--column", dest="column", default="tweet",
 	    help="the CSV column to be cleaned and saved",)
-    parser.add_option("-o", "--output",
-	    action="store", # optional because action defaults to "store"
-	    dest="output",
-	    default="tweets_clean.txt",
+    parser.add_option("-o", "--output", dest="output", default="tweets_clean.txt",
 	    help="the output text file to be saved",)
     (options, args) = parser.parse_args()
 
